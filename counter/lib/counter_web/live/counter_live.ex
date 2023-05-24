1273d673c9cbc9744c2fd2d223ffba79240214db
Jechol Lee <mr.jechol@gmail.com>
2023-05-24 23:54:20 +0900

    counter.jechol.com

--- counter/lib/counter_web/live/counter_live.ex
+++ counter/lib/counter_web/live/counter_live.ex
@@ -3,15 +3,13 @@ defmodule CounterWeb.CounterLive do
   use CounterWeb, :live_view
 
   def mount(_params, _session, socket) do
-    region = System.get_env("FLY_REGION") || "local"
-
     if connected?(socket) do
       Phoenix.PubSub.subscribe(Counter.PubSub, "counter")
     end
 
     Agent.start(fn -> 0 end, name: {:global, :counter})
     count = Agent.get({:global, :counter}, fn x -> x end)
-
+    region = System.get_env("FLY_REGION") || "local"
     {:ok, assign(socket, count: count, region: region)}
   end
 
@@ -27,29 +25,18 @@ defmodule CounterWeb.CounterLive do
   end
 
   def handle_event("increment", _params, socket) do
-    count =
-      Agent.get_and_update({:global, :counter}, fn x ->
-        Phoenix.PubSub.broadcast(Counter.PubSub, "counter", m = {:count, x + 1})
-        Logger.info("#{Node.self()}: broadcast #{inspect(m)}")
-        {x + 1, x + 1}
-      end)
-
+    count = Agent.get_and_update({:global, :counter}, fn x -> {x + 1, x + 1} end)
+    Phoenix.PubSub.broadcast(Counter.PubSub, "counter", {:count, count})
     {:noreply, assign(socket, count: count)}
   end
 
   def handle_event("decrement", _params, socket) do
-    count =
-      Agent.get_and_update({:global, :counter}, fn x ->
-        Phoenix.PubSub.broadcast(Counter.PubSub, "counter", m = {:count, x - 1})
-        Logger.info("#{Node.self()}: broadcast #{inspect(m)}")
-        {x - 1, x - 1}
-      end)
-
+    count = Agent.get_and_update({:global, :counter}, fn x -> {x - 1, x - 1} end)
+    Phoenix.PubSub.broadcast(Counter.PubSub, "counter", {:count, count})
     {:noreply, assign(socket, count: count)}
   end
 
-  def handle_info(m = {:count, count}, socket) do
-    Logger.info("#{Node.self()}: handle_info #{inspect(m)}")
+  def handle_info({:count, count}, socket) do
     {:noreply, assign(socket, count: count)}
   end
 end
