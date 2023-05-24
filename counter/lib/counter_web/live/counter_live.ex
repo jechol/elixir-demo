f795d88a501a0ec6e024956019543e4963ed448a
Jechol Lee <mr.jechol@gmail.com>
2023-05-24 23:06:26 +0900

    logging

--- counter/lib/counter_web/live/counter_live.ex
+++ counter/lib/counter_web/live/counter_live.ex
@@ -1,4 +1,5 @@
 defmodule CounterWeb.CounterLive do
+  require Logger
   use CounterWeb, :live_view
 
   def mount(_params, _session, socket) do
@@ -28,7 +29,8 @@ defmodule CounterWeb.CounterLive do
   def handle_event("increment", _params, socket) do
     count =
       Agent.get_and_update({:global, :counter}, fn x ->
-        Phoenix.PubSub.broadcast(Counter.PubSub, "counter", {:count, x + 1})
+        Phoenix.PubSub.broadcast(Counter.PubSub, "counter", m = {:count, x + 1})
+        Logger.info("#{Node.self()}: broadcast #{inspect(m)}")
         {x + 1, x + 1}
       end)
 
@@ -38,14 +40,16 @@ defmodule CounterWeb.CounterLive do
   def handle_event("decrement", _params, socket) do
     count =
       Agent.get_and_update({:global, :counter}, fn x ->
-        Phoenix.PubSub.broadcast(Counter.PubSub, "counter", {:count, x - 1})
+        Phoenix.PubSub.broadcast(Counter.PubSub, "counter", m = {:count, x - 1})
+        Logger.info("#{Node.self()}: broadcast #{inspect(m)}")
         {x - 1, x - 1}
       end)
 
     {:noreply, assign(socket, count: count)}
   end
 
-  def handle_info({:count, count}, socket) do
+  def handle_info(m = {:count, count}, socket) do
+    Logger.info("#{Node.self()}: handle_info #{inspect(m)}")
     {:noreply, assign(socket, count: count)}
   end
 end
